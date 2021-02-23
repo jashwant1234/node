14ae87f2bd81627641e5d11fa1bc7f670d8b0c6b
jashwant1234 <jpradhan727@gmail.com>
2021-02-22 23:51:49 -0800

    add mongodb

--- nodejs/app.js
+++ nodejs/app.js
@@ -13,18 +13,17 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
+const User = require('./models/user');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById("60340f21b20be8fcd6108b82")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
--- nodejs/models/user.js
+++ nodejs/models/user.js
@@ -1,16 +1,29 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
+class User {
+ 
+  constructor(username,email){
+    this.username = username;
+    this.email = email;
+  }
 
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
-
+  save(){
+    const db = getDb();
+   return db.collection("user").insertOne(this).then(()=>{
+     console.log("one user insert");
+   }).catch(err=>{
+     console.log(err);
+   });
+  }
+ 
+  static findById(userId){
+    const db = getDb();
+    return db.collection("user").find({_id : new mongodb.ObjectId(userId)}).next().then(result => {
+      console.log(result);
+      return result;
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+}
 module.exports = User;
