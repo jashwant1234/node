b92875cdb7019e5e77e2a67018dfe989ba567b49
jashwant1234 <jpradhan727@gmail.com>
2021-02-28 11:06:44 -0800

    add mongoose

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,23 +3,44 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+// const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+'mongodb://localhost:27017/shop';
+  
 const app = express();
+// const store = new MongoDBStore({
+//   uri: MONGODB_URI,
+//   collection: 'sessions'
+// });
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    // store: store
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById('603b443674250e1010914efc')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -29,15 +50,12 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-// mongodb+srv://new-user_99:jashu1432@cluster0.sf8yh.mongodb.net/test?retryWrites=true&w=majority
-
 mongoose
-  .connect(
-    'mongodb://localhost:27017/shop'
-  )
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
@@ -51,9 +69,8 @@ mongoose
         user.save();
       }
     });
-    console.log("conncted");
     app.listen(3000);
   })
   .catch(err => {
     console.log(err);
-  }); 
+  });
