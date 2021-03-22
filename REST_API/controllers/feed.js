26598909f1a68d211ea7393e5caf41d8fd6cf57e
jashwant1234 <jpradhan727@gmail.com>
2021-03-22 22:40:32 +0530

    add some frontend

--- REST_API/controllers/feed.js
+++ REST_API/controllers/feed.js
@@ -1,14 +1,22 @@
-exports.getPosts = (req,res,next) =>{
-    res.status(200).json(
-        {post : [{titel : "my name is" , content: "This is the first post"}]}
-    )
+exports.getPosts = (req, res, next) => {
+  res
+    .status(200)
+    .json({
+      post: [
+        {
+          titel: "my name is",
+          content: "This is the first post",
+          imageUrl: "images/1.jpeg",
+        }
+      ]
+    });
 };
 
-exports.createPosts = (req,res,next) => {
-    const title = req.body.title;
-    const content = req.body.content;
-    res.status(201).json({
-        message : "creted sucsfuly",
-        post : {id : new Date().toISOString(), title : title , content : content }
-    });
-}
+exports.createPosts = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  res.status(201).json({
+    message: "creted sucsfuly",
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
+};
