011bb8ddf5b5b2e59f67be579908ad5de6f2616c
jashwant1234 <jpradhan727@gmail.com>
2021-03-25 23:39:04 +0530

    add new user controller

--- REST_API/controllers/auth.js
+++ REST_API/controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 const { validationResult } = require("express-validator");
-
+const bcrypt = require('bcryptjs');
 
 exports.sginup = (req,res,next) => {
     const errors = validationResult(req);
@@ -13,5 +13,24 @@ exports.sginup = (req,res,next) => {
     const email = req.body.email;
     const password = req.body.password;
     const name = req.body.name;
-    
-}
+    bcrypt.hash(password,12).then( hasPw => {
+        const user = new User({
+            email : email,
+            password : hasPw,
+            name : name
+        });
+        return user.save();
+    })
+    .then( result => {
+        res.status(201)
+        .json({
+            message : 'User was created',
+            userId : result._id
+        })
+    }).catch( err => {
+        return res.status(422).json({
+            message : "some issue with saving",
+            errors : err
+        });
+    });
+};
--- REST_API/models/user.js
+++ REST_API/models/user.js
@@ -16,7 +16,7 @@ const userSchema = new Schema({
   },
   status: {
     type: String,
-    required: true,
+    defult : 'I am New',
   },
   post: [
     {
