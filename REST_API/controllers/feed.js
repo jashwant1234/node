36cde0032bc4327a3516ce614d6b475462fb3d2a
jashwant1234 <jpradhan727@gmail.com>
2021-03-25 18:34:56 +0530

    add pagination to api

--- REST_API/controllers/feed.js
+++ REST_API/controllers/feed.js
@@ -1,14 +1,28 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
 //fatching all post
 exports.getPosts = (req, res, next) => {
-  //console.log('here');
+  //pagination
+  const currentpage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .skip((currentpage - 1) * perPage)
+        .limit(perPage);
+    })
     .then((posts) => {
-      res.status(200).json({ posts: posts });
+      return res.status(200).json({
+        message: "Fetch successfully.",
+        post: posts,
+        totalItems: totalItems,
+      });
     })
     .catch((err) => {
       return res.status(500).json({
@@ -79,10 +93,8 @@ exports.getPostsById = (req, res, next) => {
       });
     });
 };
-
 //update a post
 exports.updatePost = (req, res, next) => {
-  
   const postId = req.params.postId;
   let imageUrl = req.body.image;
   const title = req.body.title;
@@ -103,18 +115,19 @@ exports.updatePost = (req, res, next) => {
           message: "Could not find a post.",
         });
       }
-      if(post.imageUrl !== imageUrl){
+      if (post.imageUrl !== imageUrl) {
         clearImage(post.imageUrl);
       }
       post.title = title;
       post.imageUrl = imageUrl;
       post.content = content;
       return post.save();
-    }).then(result => {
+    })
+    .then((result) => {
       return res.status(200).json({
         message: "Post updated!",
-        post: result
-      })
+        post: result,
+      });
     })
     .catch((err) => {
       return res.status(500).json({
@@ -123,33 +136,35 @@ exports.updatePost = (req, res, next) => {
     });
 };
 
-exports.deletePost = (req,res,nex) =>{
-  const postId=req.params.postId;
+//delete a post
+exports.deletePost = (req, res, nex) => {
+  const postId = req.params.postId;
   Post.findById(postId)
-  .then( post => {
-    //check logged in user
-    if (!post) {
-      return res.status(404).json({
-        message: "Could not find a post.",
-      });
-    }
-    clearImage(post.imageUrl);
-    return Post.findByIdAndRemove(postId);
-  })
-  .then( result => {
-    return res.status(200).json({
-      message: "Post deleted!",
-      post: result
+    .then((post) => {
+      //check logged in user
+      if (!post) {
+        return res.status(404).json({
+          message: "Could not find a post.",
+        });
+      }
+      clearImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
     })
-  })
-  .catch( err =>{
-    return res.status(500).json({
-      error: err,
+    .then((result) => {
+      return res.status(200).json({
+        message: "Post deleted!",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        error: err,
+      });
     });
-  })
-}
+};
 
-const clearImage = filePath => {
-  filePath = path.join(__dirname,'..', filePath);
-  fs.unlink(filePath,err=> console.log(err));
-}
+//delete the image from the file
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
+};
